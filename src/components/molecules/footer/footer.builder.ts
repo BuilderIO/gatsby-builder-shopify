b2f4ff03e2a9f82f5e44eee3f312ea42e061dcd5
Madison Zablocki <zablockimadison@gmail.com>
2020-10-27 12:00:55 -0400

    added footer

---
+++ src/components/molecules/footer/footer.builder.ts
@@ -0,0 +1,18 @@
+import { Builder, builder } from '@builder.io/react';
+import loadable from '@loadable/component';
+
+builder.init(process.env.BUILDER_API_KEY!);
+
+const LazyFooter = loadable(() => import('./footer'));
+
+Builder.registerComponent(LazyFooter, {
+  name: 'Footer',
+  description: 'Used at the bottom of the page',
+  inputs: [
+    {
+      name: 'footerTitle',
+      type: 'text',
+      defaultValue: 'Builder.io footer',
+    },
+  ],
+});
